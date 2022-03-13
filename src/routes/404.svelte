820a18f70fe2d7d7b9c5521689433482e077bf59
KingMarine <61825575+KingMarine-GH@users.noreply.github.com>
2022-03-13 22:47:28 +0800

    added temporary 404

---
+++ src/routes/404.svelte
@@ -0,0 +1,27 @@
+<script lang="ts" context="module">
+    import type { Load } from "@sveltejs/kit";
+
+    export const load: Load = async ({ url }) => {
+        // prevent some sort of exploit, forgot the name
+        let link = url.searchParams.get("link") ?? "";
+
+        if (link?.match(/^http/)) {
+            link = "";
+        }
+
+        return {
+            status: 200,
+            props: {
+                url: new URL(`https://${url.hostname}${link}`)
+            }
+        };
+    };
+</script>
+
+<script lang="ts">
+    export let url: URL;
+</script>
+
+<h1>link doesn't exist</h1>
+
+<p>You tried to access <code>{url.pathname}</code>, you sure you typed it right?</p>
