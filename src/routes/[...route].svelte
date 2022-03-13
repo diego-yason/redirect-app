bfaecb35b000b21c216e0fa06e84f92ff3983415
KingMarine <61825575+KingMarine-GH@users.noreply.github.com>
2022-03-13 23:38:01 +0800

    maybe fixed issue on security cert

---
+++ src/routes/[...route].svelte
@@ -0,0 +1,9 @@
+<script lang="ts">
+    export let redirect: string;
+
+    import { onMount } from "svelte";
+    import { goto } from "$app/navigation";
+    onMount(() => {
+        goto(redirect);
+    });
+</script>
--- src/routes/[...route].ts
+++ src/routes/[...route].ts
@@ -1,6 +1,8 @@
 import redis from "$lib/redis";
 import type { RequestHandler } from "@sveltejs/kit";
 
+import { dev } from "$app/env";
+
 export const get: RequestHandler = async ({ url }) => {
     const errorParams = new URLSearchParams({
         link: url.pathname
@@ -8,7 +10,7 @@ export const get: RequestHandler = async ({ url }) => {
 
     console.log("Trying to access %s...", url.pathname);
 
-    let link = await redis.hget("2011482.xyz", `${url.pathname}`);
+    let link = await redis.hget(dev ? "2011482.xyz" : url.hostname, `${url.pathname}`);
 
     if (!link) {
         console.error("Fail. Redirecting to 404");
@@ -18,9 +20,9 @@ export const get: RequestHandler = async ({ url }) => {
     }
 
     return {
-        status: 302,
-        headers: {
-            Location: link
+        status: 200,
+        body: {
+            redirect: link
         }
     };
 };
